ac34069f66e9b67b97b3d08027e1adb78f105350
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-11-09 18:51:30 +0200

    Created GUI for volume conventer

---
+++ ossp_conventer_view_tk.py
@@ -0,0 +1,43 @@
+
+import tkinter as tk
+
+class VolumeView:
+    def __init__(self, root, controller, units):
+        self.root = root
+        self.controller = controller
+        self.units = units
+
+        self.root.title("Конвертер об'єму")
+        self.root.geometry("500x400")
+        self.root.configure(bg="gray")
+
+        tk.Label(self.root, text="Конвертер об'єму", font=("Arial", 16, "bold"), bg="black").pack(pady=10)
+
+        tk.Label(self.root, text="Введіть число:", bg="gray").pack()
+        self.value_entry=tk.Entry(self.root, width=15, justify="center")
+        self.value_entry.pack(pady=3)
+
+        tk.Label(self.root, text="Звідки:", bg="gray").pack()
+        self.from_var=tk.StringVar(self.root)
+        self.from_var=set(units[0])
+        tk.OptionMenu(self.root, self.from_var, *units).pack(pady=3)
+
+        tk.Label(self.root, text="Куди:", bg="gray").pack()
+        self.to_var = tk.StringVar(self.root)
+        self.to_var = set(units[1])
+        tk.OptionMenu(self.root, self.to_var, *units).pack(pady=3)
+
+        tk.Button(self.root, text="Конвертувати", bg="blue", fg="white", font=("Arial", 10, "bold"), command=self.on_convert).pack(pady=10)
+
+        self.result_label=tk.Label(self.root, text="", font=("Arial", 12, "bold"), bg="black")
+        self.result_label.pack(pady=5)
+
+    def on_convert(self):
+        value_str=self.value_entry.get()
+        from_unit=self.from_var.get()
+        to_unit=self.to_var.get()
+
+        self.controller.convert(value_str, from_unit, to_unit)
+
+    def update_result(self, text: str, color: str):
+        self.result_label.config(text=text, fg=color)
